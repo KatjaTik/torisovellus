ded5fc9e41b1fc2e069fb2eaec89a472b49c83ab
katja <t9tika01@students.oamk.fi>
2021-10-06 12:35:32 +0300

    second commit

--- index.js
+++ index.js
@@ -1,37 +1,147 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+const app = express()
+const passport = require('passport')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcryptjs')
+const BasicStrategy = require('passport-http').BasicStrategy;
+app.use(bodyParser.json());
 var cloudinary = require('cloudinary');
 var cloudinaryStorage = require('multer-storage-cloudinary');
-var express = require('express');
 var multer = require('multer');
+let today = new Date().toISOString().slice(0, 10)
+let userDb = [];
+let postDb = [];
 
-var app = express();
-
-// Config cloudinary storage for multer-storage-cloudinary
-var storage = cloudinaryStorage({
-  cloudinary: cloudinary,
-  folder: '', // give cloudinary folder where you want to store images
-  allowedFormats: ['jpg', 'png'],
+var storage =  cloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: '',
+    allowedFormats: ['jpg', 'png'],
 });
 
-var parser = multer({ storage: storage });
+var parser = multer({ storage: storage});
 
-// You can store key-value pairs in express, here we store the port setting
 app.set('port', (process.env.PORT || 80));
 
-// Simple hello world route
-app.get('/', function(req, res) {
-    res.send("Hello world");
-});
-
-// POST route for reciving the uploads. multer-parser will handle the incoming data based on the 'image' key
-// Once multer has completed the upload to cloudinary, it will come to the handling function
-// below, which then sends the 201 (CREATED) response. Notice that error handling has not been properly implemented.
-app.post('/upload', parser.single('image'), function (req, res) {
-    console.log(req.file);
-    res.status(201);
+app.post('/createpost', parser.single('image'), (req, res) => {
+    postsDb.push({ title: req.body.title, 
+                category: req.body.category,  
+                location: req.body.location, 
+                price: req.body.price,
+                picture: req.body.file,
+                dateOfPost: today })
+    res.sendStatus(201);
     res.json(req.file);
-});
+  })
 
-// start listening for incoming HTTP connections
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-});
+passport.use(new BasicStrategy(
+    (username, password, done) => {
+        console.log('Basic strategy params, username: ' + username + " , password: " + password)
+        
+        //credential check
+       //search userDb for matching user and password
+        
+       const searchResult = userDb.find(user => {
+           if(username === user.username) {
+               if(bcrypt.compareSync(password, user.password)) {
+                   return true;
+               }
+           }
+        return false;
+        })
+        if(searchResult != undefined) {
+            done(null, searchResult); //successfully authenticated
+        } else {
+            done(null, false);//no credential match
+        }
+        
+    }
+));
+app.get('/', (req, res) => {
+  res.send('Tervetuloa Tori.fi:n halpaan kopioon')
+})
+
+//app.get('/protectedResource', passport.authenticate('basic', {session: false}), (req, res) => {
+    
+
+    //this api resource is now protected with http basic
+    
+ //   res.send('Successfully accessed protected resource!');
+//})
+
+app.get('/deletepost', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const post = postDb.find(p => p.id === req.params.id);
+    if(post === undefined) {
+        res.sendStatus(404);
+    }else{
+        res.json(post);
+    }
+}) 
+
+
+/*This route will recieve data structure:
+    {
+     "username": "foo",
+     "password": "bar",
+     "email": "foo@bar.com",
+    
+    }
+*/
+app.get('/posts', (req, res) => {
+    console.log('Printing all posts..');
+    res.send(postDb);
+})
+
+app.post('/signup', (req, res) => {
+    
+    console.log('original password ' + req.body.password);
+    const salt = bcrypt.genSaltSync(6);
+    console.log('salt' + salt);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    console.log('hashed password');
+    console.log(hashedPassword);
+    const newUser = {
+        username: req.body.username,
+        password: hashedPassword,
+        email: req.body.email,
+    }
+
+    userDb.push(newUser);
+    res.sendStatus(201);
+})
+
+/**JWT implemention below */
+const jwt = require('jsonwebtoken');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtSecretKey = "mySecretKey";
+const secrets = require('./secrets.json');
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: secrets.jwtSignKey
+
+};
+
+passport.use(new JwtStrategy(options, (payload, done) => {
+    //do something with the payload
+
+    //pass the control to the handler methods
+    done(null, {});
+
+}));
+
+app.post('/login', passport.authenticate('basic', {session: false}), (req, res) => {
+//create a JWT for the client
+const token = jwt.sign({foo: "bar"}, secrets.jwtSignKey);
+
+//send the JWT to the client
+res.json({ token: token })
+})
+
+//app.get('/jwtProtectedResource', passport.authenticate('jwt', {session: false}), (req, res) => {
+//    res.send("You successfully accessed JWT protected API resource");
+//})
+
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening at`, app.get('port'));
+})
